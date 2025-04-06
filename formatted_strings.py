547f40f0cb9a9ebd5ca3f8bcbd1e30ef3b797bd5
PriyadharshiniArangan <priyadharshini2012@gmail.com>
2025-04-06 22:49:40 +0200

    variables, fstrings

---
+++ formatted_strings.py
@@ -0,0 +1,13 @@
+name = "shru"
+country = "India"
+city = "Mumbai"
+letter = f'Hey my name is {name} & Iam from {country} [{city}]'
+print(letter)
+
+
+
+#string methods
+
+course = "Python for Beginners"
+print(len(course))
+
--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,44 @@
 print("Hello World")
+
+# Variables
+Name = "John"
+Age = 20
+Status = "New"
+
+print(Name, Age, Status)
+
+#input
+name = input("What is your name?   ")
+print ('Hi' , name)
+print ("hi  " + name)
+
+name = input("Hi welcome, what is your name?  ")
+colour = input ("what is your favourite colour?  ")
+print (name + '  likes  ' + colour)
+
+birth_yr = input("Whats your birth year?  ")
+age = 2025 - int(birth_yr)
+print("Your age is " , age)
+
+weight = input('What is your weight in pounds?  ')
+weight_kg = int(weight) * 0.45
+print("Your weight is " , weight_kg, 'kg')
+
+
+course = '''Python's course for begginers 
+Learn everyday
+Practice evryday'''
+print(course)
+print(course[0:])
+print(course[:])
+print(course[1:])
+print(course[1:3])
+print(course[-1])
+print(course[-2])
+print(course[1:-1])
+
+
+
+
+
+
