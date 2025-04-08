3b37153c44630a595c1e1e052ea9a6bb64bcdcc2
PriyadharshiniArangan <priyadharshini2012@gmail.com>
2025-04-08 13:39:05 +0200

    print

--- formatted_strings.py
+++ formatted_strings.py
@@ -11,3 +11,13 @@ print(letter)
 course = "Python for Beginners"
 print(len(course))
 
+#print
+print('Hello future programmer');\
+print("Python is easy to learn");\
+print("Strings dont get executed as code")
+print("Hello \nfuture programmer! \nWelcome to the world of python")
+print("Hi" , "John", "Where", "is", "Smith?")
+print('Hello future programmer.', end='')
+print(" Python is easy to learn")
+print("Hello" + " " "future" + """Python")
+      
