6106fe9d0cbe433ef816ca02c038693d630193e2
PriyadharshiniArangan <priyadharshini2012@gmail.com>
2025-04-08 14:12:08 +0200

    comparison operator

--- formatted_strings.py
+++ formatted_strings.py
@@ -19,5 +19,23 @@ print("Hello \nfuture programmer! \nWelcome to the world of python")
 print("Hi" , "John", "Where", "is", "Smith?")
 print('Hello future programmer.', end='')
 print(" Python is easy to learn")
-print("Hello" + " " "future" + """Python")
+
       
+#Comparison operators
+print(2==2)
+print(2 == 4)
+print("Hello!" == "Hello!")
+print("Hello!" == "Hi!")
+print(4 == (2 * 2))
+print (4 > 2)
+print(2 > 4)
+print(2 > 2)
+cost_of_apple = 2
+cost_of_banana = 3
+print(cost_of_apple < cost_of_banana)
+print(cost_of_apple <= cost_of_banana)
+print(cost_of_apple >= cost_of_banana)
+
+
+
+
