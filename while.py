e5bd9c58a7d22f011517b928dd50cac35451fdd8
PriyadharshiniArangan <priyadharshini2012@gmail.com>
2025-04-09 15:23:18 +0200

    while

---
+++ while.py
@@ -0,0 +1,21 @@
+i = 2
+while True:
+    if i%3 == 0:
+        break
+    print(i)
+    i += 2
+
+
+candies= 5
+
+while candies > 0:
+    print("Eating a candy")
+    candies -=1
+
+i = 10
+while i <= 15:
+    print(i)
+    i += 1 
+
+
+
